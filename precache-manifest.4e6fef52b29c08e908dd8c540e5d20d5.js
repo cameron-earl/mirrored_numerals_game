01875b52188d3ecbd6e65c5bcc4eadc773e2c22e
Cameron Earl <cameron.earl@stgconsulting.com>
2021-02-06 21:17:07 -0700

    Updates

---
+++ precache-manifest.4e6fef52b29c08e908dd8c540e5d20d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db770f0338b2b6d0703c2f3137afa084",
+    "url": "/mirrored_numerals_game/index.html"
+  },
+  {
+    "revision": "d9b78459f6cdc6803398",
+    "url": "/mirrored_numerals_game/static/css/main.19492f53.chunk.css"
+  },
+  {
+    "revision": "c4c00b91408950adb7f3",
+    "url": "/mirrored_numerals_game/static/js/2.11c5d942.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mirrored_numerals_game/static/js/2.11c5d942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b78459f6cdc6803398",
+    "url": "/mirrored_numerals_game/static/js/main.df85b35d.chunk.js"
+  },
+  {
+    "revision": "22191eaade1c743b8283",
+    "url": "/mirrored_numerals_game/static/js/runtime-main.98d42e70.js"
+  }
+]);
